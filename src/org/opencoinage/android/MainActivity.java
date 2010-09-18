6d47881e8e35e8b012264ca22ebae3cbfb9b7faa
Arto Bendiken <arto@bendiken.net>
2010-09-18 18:50:36 +0200

    Implemented a MainActivity#getVersion() method.

--- src/org/opencoinage/android/MainActivity.java
+++ src/org/opencoinage/android/MainActivity.java
@@ -4,10 +4,24 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
-  /** Called when the activity is first created. */
+  /**
+   * Called when the activity is first created.
+   */
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
+  }
+
+  /**
+   * Returns the application version number string.
+   */
+  public String getVersion() {
+    String version = "x.y.z";
+    try {
+      version = getPackageManager().getPackageInfo("org.opencoinage.android", 0).versionName;
+    }
+    catch (android.content.pm.PackageManager.NameNotFoundException e) {}
+    return version;
   }
 }
