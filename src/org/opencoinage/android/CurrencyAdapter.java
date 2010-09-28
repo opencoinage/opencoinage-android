9c2d12999e0fdc6ffcb97766a8d03ce8a72c6c34
Arto Bendiken <arto@bendiken.net>
2010-09-28 06:50:54 +0200

    Implemented the loading of currency descriptions from bundled static assets.

--- src/org/opencoinage/android/CurrencyAdapter.java
+++ src/org/opencoinage/android/CurrencyAdapter.java
@@ -8,33 +8,46 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class CurrencyAdapter extends SimpleAdapter {
-  public static final String KEY_LABEL   = "label";
-  public static final String KEY_BALANCE = "balance";
+import android.content.res.AssetManager;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
-  public static final String[][] CURRENCIES = new String[][] {
-    new String[] { "GAU", "Gold Grams",     "0.00gg" },
-    new String[] { "MMX", "Monopoly Money", "\u00A30.00" },
-    new String[] { "BBX", "Bernanke Bucks", "$0.00" },
-  };
+public class CurrencyAdapter extends SimpleAdapter {
+  public static final String CURRENCY_DIR = "currencies";
+  public static final String KEY_LABEL    = "label";
+  public static final String KEY_BALANCE  = "balance";
 
   public static List<Map<String, String>> currencies;
 
-  public static List<Map<String, String>> getCurrencies() {
+  public static List<Map<String, String>> getCurrencies(Context context) {
     if (currencies == null) {
       currencies = new ArrayList<Map<String, String>>();
-      for (int i = 0; i < CURRENCIES.length; i++) {
-        Map<String, String> currency = new HashMap<String, String>();
-        currency.put(KEY_LABEL,   CURRENCIES[i][1]);
-        currency.put(KEY_BALANCE, CURRENCIES[i][2]);
-        currencies.add(currency);
+      AssetManager assets = context.getAssets();
+      try {
+        for (String fileName: assets.list(CURRENCY_DIR)) {
+          BufferedReader in = new BufferedReader(new InputStreamReader(
+            assets.open(CURRENCY_DIR + "/" + fileName), "UTF-8"), 1024);
+          String line;
+          if ((line = in.readLine()) != null) {
+            String[] columns = line.split(";", 3);
+            Map<String, String> currency = new HashMap<String, String>();
+            currency.put(KEY_LABEL,   columns[1]);
+            currency.put(KEY_BALANCE, columns[2]);
+            currencies.add(currency);
+          }
+        }
+      }
+      catch (IOException e) {
+        e.printStackTrace(); // FIXME
       }
     }
     return currencies;
   }
 
   public CurrencyAdapter(Context context) {
-    super(context, getCurrencies(), android.R.layout.simple_list_item_2,
+    super(context, getCurrencies(context), android.R.layout.simple_list_item_2,
       new String[] { KEY_LABEL, KEY_BALANCE },
       new int[] { android.R.id.text1, android.R.id.text2 });
   }
